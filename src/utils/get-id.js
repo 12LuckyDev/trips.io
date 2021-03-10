121628765acf8f7500f22571c5e19ed41bb52dac
12LuckyDev <12luckydev@gmail.com>
2021-03-10 15:12:30 +0100

    Change id generation method

--- src/utils/get-id.js
+++ src/utils/get-id.js
@@ -1,19 +1,5 @@
-import { randomBytes } from "crypto";
+import { v4 as uuid } from "uuid";
 
-const CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-const getId = () => {
-  let autoId = "";
-  while (autoId.length < 20) {
-    const bytes = randomBytes(40);
-    bytes.forEach((b) => {
-      const maxValue = 62 * 4 - 1;
-      if (autoId.length < 20 && b <= maxValue) {
-        autoId += CHARS.charAt(b % 62);
-      }
-    });
-  }
-  return autoId;
-};
+const getId = () => uuid().replaceAll("-", "");
 
 export default getId;
